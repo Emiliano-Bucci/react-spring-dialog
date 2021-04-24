0bcbe8fc1cacee3612bd4a0ed655fc6ea2c78139
Emiliano-Bucci <emiliano.bucci@outlook.com>
2021-04-24 11:23:57 +0200

    rollup: remove common plugin

--- rollup.config.js
+++ rollup.config.js
@@ -3,7 +3,6 @@ import resolve from '@rollup/plugin-node-resolve'
 import external from 'rollup-plugin-peer-deps-external'
 import rollupTS from 'rollup-plugin-typescript2'
 import { terser } from 'rollup-plugin-terser'
-import commonjs from '@rollup/plugin-commonjs'
 
 const globals = {
   react: 'React',
@@ -35,7 +34,6 @@ export default {
     }),
     external(),
     resolve(),
-    commonjs(),
     terser(),
   ],
 }
