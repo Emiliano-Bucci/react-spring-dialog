4096e6293b60a05265600519470e5a48743e3fa7
Emiliano-Bucci <emiliano.bucci@outlook.com>
2021-04-26 18:32:25 +0200

    feat: add commonjs rollup plugin

--- rollup.config.js
+++ rollup.config.js
@@ -4,6 +4,7 @@ import external from 'rollup-plugin-peer-deps-external'
 import rollupTS from 'rollup-plugin-typescript2'
 import { terser } from 'rollup-plugin-terser'
 import size from 'rollup-plugin-filesize'
+import commonjs from '@rollup/plugin-commonjs'
 
 const globals = {
   react: 'React',
@@ -33,6 +34,7 @@ export default {
       exclude: 'node_modules/**',
       presets: ['@babel/preset-react'],
     }),
+    commonjs(),
     external(),
     resolve(),
     terser(),
